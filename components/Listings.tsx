a38133e5ff5ff6646216aee3eb397f2ec247f533
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-06-20 18:21:27 +0530

    feat: listing card

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,88 +1,87 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import ListingCard from "@/components/ListingCard";
-import Listings from "@/components/Listings";
 
 const mockData = [
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
     price: 200,
   },
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
     price: 200,
   },
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
     price: 200,
   },
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
     price: 200,
   },
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
     price: 200,
   },
   {
-    medium_url: "../../assets/images/home1.webp",
+    medium_url: "../assets/images/home1.webp",
     name: "Cozy Apartment",
     review_scores_rating: 90,
     room_type: "Entire home/apt",
     price: 120,
   },
   {
-    medium_url: "../../assets/images/home2.webp",
+    medium_url: "../assets/images/home2.webp",
     name: "Modern House",
     review_scores_rating: 96,
     room_type: "Private room",
@@ -90,8 +89,15 @@ const mockData = [
   },
 ];
 
-const Page: React.FC = () => {
-  return <Listings />;
+const Listings: React.FC = () => {
+  return (
+    <FlatList
+      data={mockData}
+      renderItem={({ item }) => <ListingCard item={item} />}
+      keyExtractor={(item, index) => index.toString()}
+      contentContainerStyle={styles.container}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
@@ -100,4 +106,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Page;
+export default Listings;
